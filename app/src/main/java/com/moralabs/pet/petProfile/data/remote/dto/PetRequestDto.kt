74a06091e98db5831c57fa19f1cc13edad66f723
Dilek Dilşah Özkan <>
2022-10-04 16:06:23 +0300

    fixes

--- app/src/main/java/com/moralabs/pet/petProfile/data/remote/dto/PetRequestDto.kt
+++ app/src/main/java/com/moralabs/pet/petProfile/data/remote/dto/PetRequestDto.kt
@@ -4,7 +4,7 @@ import com.moralabs.pet.core.data.remote.dto.BaseDto
 import com.moralabs.pet.newPost.data.remote.dto.MediaDto
 
 data class PetRequestDto (
-    val name: String? = null,
     var media: List<MediaDto>? = null,
+    val name: String? = null,
     val petAttributes: List<PetAttributeDto>? = null
 ): BaseDto()
