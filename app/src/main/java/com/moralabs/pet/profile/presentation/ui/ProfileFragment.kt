009387a38f3a7d057164f7add1505a31b201f3fc
Levent <levent.yapar@topkapidanismanlik.com.tr>
2022-10-07 10:06:21 +0300

    edit user info - profile blocked user ui

--- app/src/main/java/com/moralabs/pet/profile/presentation/ui/ProfileFragment.kt
+++ app/src/main/java/com/moralabs/pet/profile/presentation/ui/ProfileFragment.kt
@@ -3,10 +3,7 @@ package com.moralabs.pet.profile.presentation.ui
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
-import android.view.ViewParent
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.Observer
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import com.bumptech.glide.Glide
@@ -19,7 +16,6 @@ import com.moralabs.pet.core.presentation.ui.BaseFragment
 import com.moralabs.pet.databinding.FragmentProfileBinding
 import com.moralabs.pet.petProfile.presentation.ui.PetFragment
 import com.moralabs.pet.profile.data.remote.dto.UserDto
-import com.moralabs.pet.profile.data.remote.dto.UserInfoDto
 import com.moralabs.pet.profile.presentation.adapter.ProfileViewPagerAdapter
 import com.moralabs.pet.profile.presentation.viewmodel.ProfileViewModel
 import com.moralabs.pet.settings.presentation.ui.SettingsActivity
@@ -38,8 +34,6 @@ class ProfileFragment : BaseFragment<FragmentProfileBinding, UserDto, ProfileVie
         activity?.intent?.getStringExtra(ProfileActivity.OTHER_USER_ID)
     }
     private var userInfo: UserDto? = null
-    private var isUserBlocked: Boolean = false
-
 
     override fun fragmentViewModel(): BaseViewModel<UserDto> {
         val viewModel: ProfileViewModel by viewModels()
@@ -55,7 +49,7 @@ class ProfileFragment : BaseFragment<FragmentProfileBinding, UserDto, ProfileVie
                 userInfo?.let { user ->
                     OtherUserActionBottomSheetFragment(
                         user.isUserFollowed,
-                        isUserBlocked,
+                        user.isBlocked,
                         this@ProfileFragment,
                         this@ProfileFragment
                     ).show(this.parentFragmentManager, "OtherUserActionBottomSheetFragment")
@@ -64,11 +58,15 @@ class ProfileFragment : BaseFragment<FragmentProfileBinding, UserDto, ProfileVie
         }
 
         binding.followedLinear.setOnClickListener {
-            findNavController().navigate(R.id.action_fragment_profile_to_followedFragment)
+            if (otherUserId.isNullOrBlank()) {
+                findNavController().navigate(R.id.action_fragment_profile_to_followedFragment)
+            }
         }
 
         binding.followerLinear.setOnClickListener {
-            findNavController().navigate(R.id.action_fragment_profile_to_followersFragment)
+            if (otherUserId.isNullOrBlank()) {
+                findNavController().navigate(R.id.action_fragment_profile_to_followersFragment)
+            }
         }
 
         binding.imgBack.setOnClickListener {
@@ -97,13 +95,12 @@ class ProfileFragment : BaseFragment<FragmentProfileBinding, UserDto, ProfileVie
         binding.userFullName.text = data.fullName.toString()
         binding.username.text = data.userName.toString()
         binding.toolbarUsername.text = data.userName.toString()
-        if (data.isUserBlockedByUser != true) {
-            setProfileUI(data)
-        } else {
+        if (data.isUserBlockedByUser == true) {
             otherUserBlockedUI()
-        }
-        if (isUserBlocked) {
+        } else if (data.isBlocked == true) {
             blockedUserUI()
+        } else {
+            setProfileUI(data)
         }
     }
 
@@ -147,28 +144,6 @@ class ProfileFragment : BaseFragment<FragmentProfileBinding, UserDto, ProfileVie
             }
         }
         lifecycleScope.launch {
-            viewModel.blockedListState.collect {
-                when (it) {
-                    is ViewState.Loading -> {
-                        startLoading()
-                    }
-                    is ViewState.Success<List<UserInfoDto>> -> {
-                        isUserBlocked = false
-                        it.data.forEach {
-                            if (it.userId == otherUserId) {
-                                isUserBlocked = true
-                            }
-                        }
-                        viewModel.otherUsersInfo(otherUserId)
-                    }
-                    is ViewState.Error<*> -> {
-                        stopLoading()
-                    }
-                    else -> {}
-                }
-            }
-        }
-        lifecycleScope.launch {
             viewModel.blockUserState.collect {
                 when (it) {
                     is ViewState.Loading -> {
@@ -193,7 +168,7 @@ class ProfileFragment : BaseFragment<FragmentProfileBinding, UserDto, ProfileVie
                         startLoading()
                     }
                     is ViewState.Success<Boolean> -> {
-                        if (it.data && isUserBlocked) {
+                        if (it.data) {
                             getUserInfo()
                         }
                     }
@@ -217,8 +192,8 @@ class ProfileFragment : BaseFragment<FragmentProfileBinding, UserDto, ProfileVie
     }
 
     private fun getUserInfo() {
-        if (!otherUserId.isNullOrBlank()) {
-            viewModel.getBlockedList()
+        if (otherUserId.isNullOrBlank().not()) {
+            viewModel.otherUsersInfo(otherUserId)
         } else {
             viewModel.userInfo()
             binding.imgBack.visibility = View.GONE
@@ -247,8 +222,9 @@ class ProfileFragment : BaseFragment<FragmentProfileBinding, UserDto, ProfileVie
 
     private fun setProfileUI(data: UserDto) {
         binding.userSocialInfo.visibility = View.VISIBLE
-        if(otherUserId.isNullOrBlank().not()) {
+        if(otherUserId.isNullOrBlank().not() && binding.viewpager.adapter == null) {
             binding.viewpager.adapter = viewPagerAdapter
+            viewPagerAdapter.notifyDataSetChanged()
         }
         binding.totalPost.text = data.postCount.toString()
         if (data.postCount == null) {
