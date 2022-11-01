f4b086cf15851ee49753cc3dc81a6a0308e0f403
Dilek Dilşah Özkan <>
2022-11-01 15:24:14 +0300

    facebook, notification yönlendirmesi

--- app/src/main/java/com/moralabs/pet/message/presentation/ui/MessageFragment.kt
+++ app/src/main/java/com/moralabs/pet/message/presentation/ui/MessageFragment.kt
@@ -28,7 +28,14 @@ class MessageFragment : BaseFragment<FragmentMessageBinding, List<ChatDto>, Mess
 
     private val messageAdapter: BaseListAdapter<ChatDto, ItemUserMessageBinding> by lazy {
         BaseListAdapter(R.layout.item_user_message, BR.item, onRowClick = {
-            startActivity(Intent(context, MessageDetailActivity::class.java).apply { putExtras(bundleOf(MessageDetailActivity.BUNDLE_USER to it.to)) })
+            startActivity(Intent(context, MessageDetailActivity::class.java).apply {
+                putExtras(
+                    bundleOf(
+                        MessageDetailActivity.BUNDLE_USER to it.to,
+                        MessageDetailActivity.USER_ID to it.to?.userId
+                    )
+                )
+            })
         }, emptyString = getString(R.string.noMessage))
     }
 
