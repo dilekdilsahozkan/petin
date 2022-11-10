1c29b919dc76f1c79417259c10c46238dbd4188c
Dilek Dilşah Özkan <>
2022-11-10 10:28:13 +0300

    facebook login

--- app/src/main/java/com/moralabs/pet/PetApp.kt
+++ app/src/main/java/com/moralabs/pet/PetApp.kt
@@ -1,7 +1,16 @@
 package com.moralabs.pet
 
 import android.app.Application
+import com.facebook.FacebookSdk
+import com.facebook.appevents.AppEventsLogger
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class PetApp  : Application()
+class PetApp  : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        FacebookSdk.sdkInitialize(applicationContext)
+        AppEventsLogger.activateApp(this)
+    }
+}
